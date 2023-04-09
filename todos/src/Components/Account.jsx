11af9e0d2a1518bf0c6cf443b4680714db1e14ef
kdb11 <kevin.badwi@medieinstitutet.se>
2023-04-09 22:44:29 +0200

    something working, but lot of errors

--- todos/src/Components/Account.jsx
+++ todos/src/Components/Account.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
 import { TODOS_LIST_ADRESS, TODO_LIST_ABI } from "../config";
+import { AddTodoForm } from "./AddTodoForm";
 
 export function Account() {
     const [account, SetAccount] = useState();
-    const [indexes, SetIndexes] = useState([]);
+    const [contract, SetContract] = useState();
+    const [todos, setTodos] = useState([]);
     
     useEffect(() => {
         const getAccounts = async () => {
@@ -15,15 +17,11 @@ export function Account() {
             SetAccount(accounts[0]);
 
             const todoContract = new web3.eth.Contract(TODO_LIST_ABI, TODOS_LIST_ADRESS);
-            
-            let indexes = await todoContract.methods.getIndexList().call();
-            
-            console.log(indexes);
 
-            const html = indexes.map((index, i) => {
-                return <li key={i}>{index}</li>;
-            });
-            SetIndexes(html)
+            SetContract(todoContract);
+
+            await populateTodos(todoContract);
+            
         };
 
 
@@ -31,11 +29,49 @@ export function Account() {
         getAccounts();
     });
 
+    const populateTodos = async (contract) => {
+        let indexes = await contract.methods.todoCount().call();
+
+        console.log(indexes);
+
+        let todoList = [];
+
+        for (let i = 0; i < indexes.length; i++) {
+          const todo = await contract.methods.todos(indexes[i]).call;
+          console.log(todo);
+          todoList.push(todo);
+        }
+
+        setTodos(todoList);
+    };
+
+    const html = todos.map((todo) => {
+        return(
+            <ul>
+            <li>{todo.id}</li>
+            <li>{todo.text}</li>
+            <li>{todo.completed}</li>
+        </ul>
+        );
+    });
+
+    const createTodo = async (todo) => {
+        await contract.methods
+        .createTodo(todo.titel)
+/*         await contract.methods.createTodo(todo.id)
+        await contract.methods.createTodo(todo.completed) */
+        .send({from: account})
+        .once("reciept", async (reciept) => {
+            console.log(reciept);
+
+            populateTodos(contract);
+        })
+    }
+    
 
     return <div>
         <p>Account: {account}</p>
-        <ul>
-           {indexes} 
-        </ul>
+        <AddTodoForm addTodo={createTodo} ></AddTodoForm>
+        {html}
     </div>
 };
---
+++ todos/src/Models/TodoItem.js
@@ -0,0 +1,11 @@
+export class TodoItem {
+    date;
+    titel;
+    stage;
+
+    constructor ( date, titel, stage) {
+        this.date = date;
+        this.titel = titel;
+        this.stage = stage;
+    };
+};
