27f0df2d5a6685470d984e658955bf8365fc81f2
kdb11 <kevin.badwi@medieinstitutet.se>
2023-04-10 22:36:10 +0200

    fix

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { Account } from './Components/Account';
 import { TodoList } from './Components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Account></Account> */}
         <TodoList></TodoList>
       </header>
     </div>
